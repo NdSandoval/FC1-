eed8606f690a8d275eff47487294673907406cc6
Dani <ldtorrest@udistrital.edu.co>
2024-03-08 00:33:48 -0500

    p

--- CalcST.cpp
+++ CalcST.cpp
@@ -1,3 +1,10 @@
 // Hola esta es la mejor calculadora en equipo
 //este es nuestro primer pull, te quiero mucho :3
 //FUNCIONA? 
+#include<cmath>
+
+double x1,x2;
+ std::cin>x1;
+ std::cin>x2;
+return(0);
+
