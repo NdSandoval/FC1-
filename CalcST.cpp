cd3e8145d768344613f19d59b2b0cac204cdb42f
Nico <ndsandovalt@udistrital.edu.co>
2024-03-08 00:25:37 -0500

    Creacion de la funcion principal

--- CalcST.cpp
+++ CalcST.cpp
@@ -1,3 +1,8 @@
 // Hola esta es la mejor calculadora en equipo
 //este es nuestro primer pull, te quiero mucho :3
 //FUNCIONA? 
+//Si, si funciona :D
+#include<iostream>
+int main(){
+    return 0;
+}
